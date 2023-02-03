d63e88d1512bd989d77817ac344c69f6d4b8a245
minsekim1 <tkarnrwl7862@naver.com>
2023-02-03 17:42:31 +0900

    console.log 추가

--- function/updateByNaverCatalog.ts
+++ function/updateByNaverCatalog.ts
@@ -37,116 +37,136 @@ const getProductByNaverCatalog = (productId: number, catalogUrl: string, index: 
         return resolve(true);
       }
       //#endregion
-      request(catalogUrl, async (error, response, body) => {
-        if (error) {
-          l("error request", "red", error);
-          resolve(true);
-          throw error;
-        }
-        let $ = cheerio.load(body);
-        try {
-          const storeList: StoreType[] = [];
-          const regex = /[^0-9]/g;
-          const review_count = Number($(`#__next > div > div > div > div > div > div > a`).text().replace(regex, ""));
-          $("#section-price > ul > li").each((i: any, item: any) => {
-            // 판매처이름
-            const idx = i + 1;
+      request(
+        catalogUrl,
+        {
+          headers: {
+            "accept-encoding": "deflate, br",
+            "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+            "cache-control": "max-age=0",
+            dnt: 1,
+            "sec-ch-ua-platform": "Mac",
+            "sec-fetch-dest": "document",
+            "user-agent": "PostmanRuntime/7.29.2",
+            Accept: "*/*",
+            Connection: "keep-alive",
+            // "sec-ch-ua-platform": "Android",
+            "User-Agent":
+              "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36",
+          },
+        },
+        (error, response, body) => {
+          if (error) {
+            l("error request", "red", error);
+            resolve(true);
+            throw error;
+          }
+          let $ = cheerio.load(body);
+          try {
+            const storeList: StoreType[] = [];
+            const regex = /[^0-9]/g;
+            const review_count = Number($(`#__next > div > div > div > div > div > div > a`).text().replace(regex, ""));
+            $("#section-price > ul > li").each((i: any, item: any) => {
+              // 판매처이름
+              const idx = i + 1;
 
-            const store_name = $(
-              `#section-price > ul > li:nth-child(${idx}) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)  > span:nth-child(1) > span:nth-child(1)`
-            ).text();
+              const store_name = $(
+                `#section-price > ul > li:nth-child(${idx}) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)  > span:nth-child(1) > span:nth-child(1)`
+              ).text();
 
-            // 판매처 제품가격
-            // #section-price > ul > li:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)  > span:nth-child(2) > span:nth-child(1)
-            const priceStr = $(
-              `#section-price > ul > li:nth-child(${idx}) > div > div > div > span > span > em`
-            ).text();
-            const price = priceStr ? Number(priceStr.replace(regex, "")) : 0;
+              // 판매처 제품가격
+              // #section-price > ul > li:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)  > span:nth-child(2) > span:nth-child(1)
+              const priceStr = $(
+                `#section-price > ul > li:nth-child(${idx}) > div > div > div > span > span > em`
+              ).text();
+              const price = priceStr ? Number(priceStr.replace(regex, "")) : 0;
 
-            //판매처 배송비
-            const deliveryStr = $(
-              `#section-price > ul > li:nth-child(${idx}) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)> span:nth-child(2)`
-            ).text();
-            const delivery = deliveryStr ? Number(deliveryStr.replace(regex, "")) : 0;
-            // 판매처링크
-            const store_link = $(`#section-price > ul > li:nth-child(${idx}) > div:nth-child(1) > a`).attr("href");
-            storeList.push({
-              product_id: productId,
+              //판매처 배송비
+              const deliveryStr = $(
+                `#section-price > ul > li:nth-child(${idx}) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)> span:nth-child(2)`
+              ).text();
+              const delivery = deliveryStr ? Number(deliveryStr.replace(regex, "")) : 0;
+              // 판매처링크
+              const store_link = $(`#section-price > ul > li:nth-child(${idx}) > div:nth-child(1) > a`).attr("href");
+              storeList.push({
+                product_id: productId,
+                store_name,
+                store_link,
+                price,
+                delivery,
+              });
+              l(
+                "GET",
+                "green",
+                `(${idx.toString().padStart(2)}) id:${productId.toString().padStart(5)} price:${price
+                  .toString()
+                  .padStart(6)}, delivery: ${delivery.toString().padStart(4)}, ${store_name}`
+              );
+              const data = { product_id: productId, store_name, store_link, store_index: idx, price, delivery };
+              axios.post("https://node2.yagiyagi.kr/product/catalog/id", data);
+            });
+            // 최저가 가져오기
+            let cheapStore: { low_price: number | null; index: number | null; data: StoreType | null } = {
+              low_price: null,
+              index: null,
+              data: null,
+            };
+            for (let index = 0; index < storeList.length; index++) {
+              const data = storeList[index];
+              const price = data.price ? data.price : 0;
+              if (cheapStore.index === null) cheapStore = { low_price: price, index, data };
+              else if (cheapStore.low_price != null && cheapStore.low_price > price)
+                cheapStore = { low_price: price, index, data };
+            }
+            // DB Insert 최저가 데이터 넣기
+            if (!cheapStore.data) {
+              l(
+                "Pass",
+                "green",
+                `no cheapStore.data, cheapStore=${JSON.stringify(cheapStore)} storeList.length=${
+                  storeList.length
+                } product_id:${productId} url=${catalogUrl}`
+              );
+              return resolve(true);
+            }
+            const { product_id, price: low_price, delivery, store_name, store_link } = cheapStore.data;
+            if (!product_id || !low_price || !delivery || !store_name || !store_link) {
+              if (!product_id) l("Pass", "green", `no product_id, product_id:${productId}`);
+              if (!low_price) l("Pass", "green", `no low_price, product_id:${productId}`);
+              if (!delivery) l("Pass", "green", `no delivery, product_id:${productId}`);
+              if (!store_name) l("Pass", "green", `no store_name, product_id:${productId}`);
+              if (!store_link) l("Pass", "green", `no store_link, product_id:${productId}`);
+              return resolve(true);
+            }
+            const data = {
+              product_id,
+              low_price,
+              delivery,
               store_name,
               store_link,
-              price,
-              delivery,
-            });
+              review_count,
+              type: "naver",
+            };
+            const idx = cheapStore.index != null ? cheapStore.index + 1 : 0;
             l(
-              "GET",
-              "green",
-              `(${idx.toString().padStart(2)}) id:${productId.toString().padStart(5)} price:${price
+              "LowPrice",
+              "cyan",
+              `[${index}/${max}] (${idx.toString().padStart(2)}) id:${productId
                 .toString()
-                .padStart(6)}, delivery: ${delivery.toString().padStart(4)}, ${store_name}`
+                .padStart(5)} price:${low_price.toString().padStart(6)}, delivery: ${delivery
+                .toString()
+                .padStart(4)}, ${store_name}`
             );
-            const data = { product_id: productId, store_name, store_link, store_index: idx, price, delivery };
-            axios.post("https://node2.yagiyagi.kr/product/catalog/id", data);
-          });
-          await wrapSlept(3000);
-          // 최저가 가져오기
-          let cheapStore: { low_price: number | null; index: number | null; data: StoreType | null } = {
-            low_price: null,
-            index: null,
-            data: null,
-          };
-          for (let index = 0; index < storeList.length; index++) {
-            const data = storeList[index];
-            const price = data.price ? data.price : 0;
-            if (cheapStore.index === null) cheapStore = { low_price: price, index, data };
-            else if (cheapStore.low_price != null && cheapStore.low_price > price)
-              cheapStore = { low_price: price, index, data };
+            axios
+              .post("https://node2.yagiyagi.kr/product/price", data)
+              .then(() => resolve(true))
+              .catch(() => resolve(true));
+          } catch (error) {
+            l("error", "red", `[${index}/${max}] id:${productId.toString().padStart(5)}`);
+            resolve(true);
           }
-          // DB Insert 최저가 데이터 넣기
-          if (!cheapStore.data) {
-            l(
-              "Pass",
-              "green",
-              `no cheapStore.data, cheapStore=${JSON.stringify(cheapStore)} storeList.length=${
-                storeList.length
-              } product_id:${productId} url=${catalogUrl}`
-            );
-            return resolve(true);
-          }
-          const { product_id, price: low_price, delivery, store_name, store_link } = cheapStore.data;
-          if (!product_id || !low_price || !delivery || !store_name || !store_link) {
-            if (!product_id) l("Pass", "green", `no product_id, product_id:${productId}`);
-            if (!low_price) l("Pass", "green", `no low_price, product_id:${productId}`);
-            if (!delivery) l("Pass", "green", `no delivery, product_id:${productId}`);
-            if (!store_name) l("Pass", "green", `no store_name, product_id:${productId}`);
-            if (!store_link) l("Pass", "green", `no store_link, product_id:${productId}`);
-            return resolve(true);
-          }
-          const data = {
-            product_id,
-            low_price,
-            delivery,
-            store_name,
-            store_link,
-            review_count,
-            type: "naver",
-          };
-          const idx = cheapStore.index != null ? cheapStore.index + 1 : 0;
-          l(
-            "LowPrice",
-            "cyan",
-            `[${index}/${max}] (${idx.toString().padStart(2)}) id:${productId.toString().padStart(5)} price:${low_price
-              .toString()
-              .padStart(6)}, delivery: ${delivery.toString().padStart(4)}, ${store_name}`
-          );
-          axios
-            .post("https://node2.yagiyagi.kr/product/price", data)
-            .then(() => resolve(true))
-            .catch(() => resolve(true));
-        } catch (error) {
-          l("error", "red", `[${index}/${max}] id:${productId.toString().padStart(5)}`);
-          resolve(true);
         }
-      });
+      );
     } catch {
       l("Error:1", "red");
       resolve(true);
