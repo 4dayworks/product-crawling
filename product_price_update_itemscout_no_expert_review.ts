fff5087cd909162b3fc6db17779d3ac2f10cad50
minsekim1 <tkarnrwl7862@naver.com>
2023-02-03 15:34:06 +0900

    🐛 yarn update:naver 수정

---
+++ product_price_update_itemscout_no_expert_review.ts
@@ -0,0 +1,8 @@
+import { updateByItemscout } from "./function/updateByItemscout";
+
+updateByItemscout(40000, 0, false);
+
+//[O] 약국제품은 is_manual=1로 고정 가격 안불러오기 / 아이템스카우트 / 네이버
+// 약사님 있는거는 매일
+// 약사님 리뷰 없으면 일주일에 한번 월요일 00시에 반복
+// 40개 묶어서 insert하기
