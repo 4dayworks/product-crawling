fc29ef7d54bb1312656036431d56ff1b7ee8ceb0
minsekim1 <tkarnrwl7862@naver.com>
2023-02-13 18:05:30 +0900

    delete test code

--- function/updateByItemscout.ts
+++ function/updateByItemscout.ts
@@ -1,8 +1,4 @@
-import {
-  ProductTable,
-  ItemscoutType,
-  ProductTableV2,
-} from "./updateByItemscout.d";
+import { ProductTable, ItemscoutType, ProductTableV2 } from "./updateByItemscout.d";
 import axios from "axios";
 import { l } from "./console";
 //식품>다이어트식품>가르시니아
@@ -15,8 +11,7 @@ const exceptCategory = (category: string) => {
 
   //2depth
   if (categoryData[1] !== "건강식품") {
-    if (categoryData[1] === "다이어트식품" && categoryData[2] === "가르시니아")
-      return true;
+    if (categoryData[1] === "다이어트식품" && categoryData[2] === "가르시니아") return true;
     return false;
   }
 
@@ -30,12 +25,7 @@ const exceptCategory = (category: string) => {
     return false;
   return true;
 };
-const getProductByItemscout = (
-  product_id: number,
-  product_name: string,
-  index: number,
-  max: number
-) =>
+const getProductByItemscout = (product_id: number, product_name: string, index: number, max: number) =>
   new Promise(async (resolve, reject) => {
     try {
       //#region product/is_drugstore = 1 이거나 product_price/is_manual = 1이면 새로 갱신 안함.
@@ -50,25 +40,14 @@ const getProductByItemscout = (
             regular_price: number | null;
             is_manual: number | null;
           }
-        | undefined = await axios(
-        `https://node2.yagiyagi.kr/product/price/manual?product_id=${product_id}`
-      ).then((d) => d.data.data);
-      if (
-        manualData &&
-        (manualData.is_drugstore === 1 || manualData.is_manual === 1)
-      ) {
+        | undefined = await axios(`https://node2.yagiyagi.kr/product/price/manual?product_id=${product_id}`).then(
+        (d) => d.data.data
+      );
+      if (manualData && (manualData.is_drugstore === 1 || manualData.is_manual === 1)) {
         if (manualData.is_drugstore === 1)
-          l(
-            "Pass",
-            "green",
-            `[${index}/${max}] is_drugstore === 1, product_id:${product_id}`
-          );
+          l("Pass", "green", `[${index}/${max}] is_drugstore === 1, product_id:${product_id}`);
         else if (manualData.is_manual === 1)
-          l(
-            "Pass",
-            "green",
-            `[${index}/${max}] is_manual === 1, product_id:${product_id}`
-          );
+          l("Pass", "green", `[${index}/${max}] is_manual === 1, product_id:${product_id}`);
         return resolve(true);
       }
       //#endregion
@@ -78,13 +57,11 @@ const getProductByItemscout = (
         keyword: string; // '그린스토어 피로한눈엔 루테인 아스타잔틴오메가3',
         keyword_id: number | null; // 349123387,
         exception_keyword: string | null;
-      } | null = await axios(
-        `https://node2.yagiyagi.kr/product/keyword/id?product_id=${product_id}`
-      ).then((d) => d.data.data);
-      let keyword_id =
-        originData && originData.keyword_id ? originData.keyword_id : null;
-      let keyword =
-        originData && originData.keyword ? originData.keyword : null;
+      } | null = await axios(`https://node2.yagiyagi.kr/product/keyword/id?product_id=${product_id}`).then(
+        (d) => d.data.data
+      );
+      let keyword_id = originData && originData.keyword_id ? originData.keyword_id : null;
+      let keyword = originData && originData.keyword ? originData.keyword : null;
       if (originData === null) {
         // (2-2). 아예 없으면 키워드 제품명으로해서 itemscout REST API 써서 keyword_id가져오고 insert하기
         // POST /product/keyword/id
@@ -154,10 +131,7 @@ const getProductByItemscout = (
         if (!d.data.data.productListResult) return [];
         return (d.data.data.productListResult as any[]).filter(
           (p: ItemscoutType) =>
-            p.isAd === false &&
-            p.isOversea === false &&
-            !isExceptionKeyword(p.title) &&
-            exceptCategory(p.category)
+            p.isAd === false && p.isOversea === false && !isExceptionKeyword(p.title) && exceptCategory(p.category)
         );
       });
 
@@ -169,32 +143,29 @@ const getProductByItemscout = (
         );
         return resolve(true);
       }
-      const storeList: ProductTableV2[] = await productListResult.map(
-        (p, i) => {
-          return {
-            index: i + 1,
-            keyword,
-            keyword_id,
-            itemscout_product_name: p.title,
-            itemscout_product_image: p.image,
-            itemscout_product_id: p.productId,
-            price: p.price,
-            store_link: p.link,
-            store_name: p.shop,
-            category: p.category,
-            is_naver_shop: p.isNaverShop === true ? 1 : 0,
-            mall: typeof p.mall === "string" ? p.mall : p.mall.join(","),
-            itemscout_mall_img: p.mallImg ? p.mallImg : null,
-            review_count: p.reviewCount,
-            review_score: p.reviewScore,
-            delivery: p.deliveryFee,
-            pc_product_url: p.pcProductUrl,
-            mobile_product_url: p.mobileProductUrl,
-            is_oversea:
-              p.isOversea === false ? 0 : p.isOversea === true ? 1 : null,
-          };
-        }
-      );
+      const storeList: ProductTableV2[] = await productListResult.map((p, i) => {
+        return {
+          index: i + 1,
+          keyword,
+          keyword_id,
+          itemscout_product_name: p.title,
+          itemscout_product_image: p.image,
+          itemscout_product_id: p.productId,
+          price: p.price,
+          store_link: p.link,
+          store_name: p.shop,
+          category: p.category,
+          is_naver_shop: p.isNaverShop === true ? 1 : 0,
+          mall: typeof p.mall === "string" ? p.mall : p.mall.join(","),
+          itemscout_mall_img: p.mallImg ? p.mallImg : null,
+          review_count: p.reviewCount,
+          review_score: p.reviewScore,
+          delivery: p.deliveryFee,
+          pc_product_url: p.pcProductUrl,
+          mobile_product_url: p.mobileProductUrl,
+          is_oversea: p.isOversea === false ? 0 : p.isOversea === true ? 1 : null,
+        };
+      });
       if (storeList && storeList.length > 0)
         await axios.post(`https://node2.yagiyagi.kr/v2/product/keyword/data`, {
           data: storeList,
@@ -212,9 +183,7 @@ const getProductByItemscout = (
       //#region (4) product_price 최종 최저가 업데이트하기
       const lowPriceObj =
         productListResult && productListResult.length
-          ? productListResult.reduce((prev, value) =>
-              prev.price <= value.price ? prev : value
-            )
+          ? productListResult.reduce((prev, value) => (prev.price <= value.price ? prev : value))
           : null;
       const idx = index + 1;
       if (!lowPriceObj) {
@@ -232,9 +201,7 @@ const getProductByItemscout = (
         low_price: lowPriceObj.price,
         delivery: lowPriceObj.deliveryFee ? lowPriceObj.deliveryFee : 0,
         store_name:
-          typeof lowPriceObj.mall !== "string" && lowPriceObj.isNaverShop
-            ? "네이버 브랜드 카탈로그"
-            : lowPriceObj.mall,
+          typeof lowPriceObj.mall !== "string" && lowPriceObj.isNaverShop ? "네이버 브랜드 카탈로그" : lowPriceObj.mall,
         store_link: lowPriceObj.link,
         review_count: lowPriceObj.reviewCount,
         type: "itemscout",
@@ -245,11 +212,9 @@ const getProductByItemscout = (
         "blue",
         `[${index}/${max}] (${idx.toString().padStart(2)}) id:${product_id
           .toString()
-          .padStart(5)} price:${data.low_price
+          .padStart(5)} price:${data.low_price.toString().padStart(6)}, delivery: ${data.delivery
           .toString()
-          .padStart(6)}, delivery: ${data.delivery.toString().padStart(4)}, ${
-          data.store_name
-        }`
+          .padStart(4)}, ${data.store_name}`
       );
 
       axios
@@ -258,11 +223,7 @@ const getProductByItemscout = (
         .catch(() => resolve(true));
       //#endregion
     } catch (error) {
-      l(
-        "error",
-        "red",
-        `[${index}/${max}] product_id:${product_id.toString().padStart(5)}`
-      );
+      l("error", "red", `[${index}/${max}] product_id:${product_id.toString().padStart(5)}`);
       console.log(error);
       resolve(true);
     }
@@ -271,28 +232,20 @@ const getProductByItemscout = (
 // 2. 아이템스카우트 데이터가져오기 & 야기 DB procude_price에 반영
 // 네이버카탈로그 url 있으면 안가져오고 넘김.
 // is_expert_review 가 있는지 없는지 비교해서 실행함.
-export const updateByItemscout = async (
-  size: number,
-  page: number,
-  is_expert_review: boolean
-) => {
+export const updateByItemscout = async (size: number, page: number, is_expert_review: boolean) => {
   // (1) 키워드 가져올 제품아이디 전체 가져오기
   // GET /product/keyword
   const d: {
     product_id: number; //34074;
     product_name: string; //'리얼메디 어린콜라겐 펩타이드 비오틴 100'
   }[] = await axios(
-    `https://node2.yagiyagi.kr/product/keyword?size=${size}&page=${page}&is_expert_review=${
-      is_expert_review ? 1 : 0
-    }`
+    `https://node2.yagiyagi.kr/product/keyword?size=${size}&page=${page}&is_expert_review=${is_expert_review ? 1 : 0}`
   ).then((d) => d.data.data);
 
   for (let i = 0; i < d.length; i++) {
     const { product_id, product_name } = d[i];
     l("timestamp", "cyan", new Date().toISOString());
-    console.log(product_id);
-    if (product_id === 21066)
-      await getProductByItemscout(product_id, product_name, i + 1, d.length);
+    await getProductByItemscout(product_id, product_name, i + 1, d.length);
   }
 
   l("[DONE]", "blue", "itemscout_keyword to product price");
