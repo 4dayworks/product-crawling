351f42c8ef8f0d9ed342c4204cecfc63a05a76e3
gun-bro98 <one0374@naver.com>
2023-03-21 14:28:33 +0900

    fixed product_price_update_naver_schedular

--- product_price_update_naver_schedular.ts
+++ product_price_update_naver_schedular.ts
@@ -10,12 +10,27 @@ axios.defaults.headers.common["Authorization"] = `Bearer ${AuthorizationKey()}`;
 const date = new Date();
 const time_type = date.getHours() < 12 ? 0 : 1;
 
+const insertGraph = async (product: getAllProductIdType) => {
+  try {
+    await axios.post(`${NODE_API_URL}/product/daily_price/history`, {
+      time_type,
+      product_id: product.product_id,
+    });
+    l("[Insert DONE]", "blue", "complete - naver product_price write history");
+  } catch (error) {
+    l("[Insert Fail]", "red", "Failed - naver product_price write history");
+  }
+};
+
 const updateByProductId = async (product_id_list?: number[]) => {
   // (1) 키워드 가져올 제품아이디 전체 가져오기
-  let data: getAllProductIdType[] = await axios(`${NODE_API_URL}/crawling/product/all`).then((d) => d.data.data);
+  let data: getAllProductIdType[] = await axios(
+    `${NODE_API_URL}/crawling/product/all`
+  ).then((d) => d.data.data);
 
   // 특정 제품만 가져오기 (없으면 전체 제품 대상)
-  if (product_id_list) data = data.filter((p) => product_id_list.includes(p.product_id));
+  if (product_id_list)
+    data = data.filter((p) => product_id_list.includes(p.product_id));
 
   for (let i = 0; i < data.length; i++) {
     const product = data[i];
@@ -25,16 +40,8 @@ const updateByProductId = async (product_id_list?: number[]) => {
     //   await wrapSlept(300);
     // } else
     if (product.type === "naver" && product.naver_catalog_link) {
-      await getProductByNaverCatalogV2(product, i + 1, data.length);
-      try {
-        await axios.post(`${NODE_API_URL}/product/daily_price/history`, {
-          time_type,
-          product_id: product.product_id,
-        });
-        l("[Insert DONE]", "blue", "complete - naver product_price write history");
-      } catch (error) {
-        l("[Insert Fail]", "red", "Failed - naver product_price write history");
-      }
+      await getProductByNaverCatalogV2(product, i + 1, data.length, true);
+      await insertGraph(product);
       await wrapSlept(2000);
     }
     l("timestamp", "cyan", new Date().toISOString());
