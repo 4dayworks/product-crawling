0de7377e9fd78e0960caefe0434604071cb05537
gun-bro98 <one0374@naver.com>
2023-03-28 16:29:27 +0900

    fixed common.ts

---
+++ function/product.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { getAllProductIdType } from "../product_price_update";
+import { NODE_API_URL } from "./common";
+import { l } from "./console";
+
+export const setGraph = async (product: getAllProductIdType) => {
+  try {
+    await axios.post(`${NODE_API_URL}/v2/product/daily_price/history`, {
+      product_id: product.product_id,
+    });
+    l("[Insert DONE]", "blue", "complete - naver product_price write history");
+  } catch (error) {
+    l("[Insert Fail]", "red", "Failed - naver product_price write history");
+  }
+};
+
+export const setLastMonthLowPrice = async (product: getAllProductIdType) => {
+  try {
+    await axios.patch(`${NODE_API_URL}/product/price/low_price`, {
+      product_id: product.product_id,
+    });
+    l("[Update Done]", "blue", "complete - low price of month was written");
+  } catch (error) {
+    l("[Update Fail]", "red", "Failed - low price of month was written");
+  }
+};
--- product_price_update.ts
+++ product_price_update.ts
@@ -6,14 +6,18 @@ import { AuthorizationKey } from "./function/auth";
 import { l } from "./function/console";
 import { wrapSlept } from "./function/wrapSlept";
 import { getProductByItemscoutV2 } from "./function/updateByItemscoutV2";
+import { setGraph, setLastMonthLowPrice } from "./function/product";
 axios.defaults.headers.common["Authorization"] = `Bearer ${AuthorizationKey()}`;
 
 const updateByProductId = async (product_id_list?: number[]) => {
   // (1) 키워드 가져올 제품아이디 전체 가져오기
-  let data: getAllProductIdType[] = await axios(`${NODE_API_URL}/crawling/product/all`).then((d) => d.data.data);
+  let data: getAllProductIdType[] = await axios(
+    `${NODE_API_URL}/crawling/product/all`
+  ).then((d) => d.data.data);
 
   // 특정 제품만 가져오기 (없으면 전체 제품 대상)
-  if (product_id_list) data = data.filter((p) => product_id_list.includes(p.product_id));
+  if (product_id_list)
+    data = data.filter((p) => product_id_list.includes(p.product_id));
 
   for (let i = 0; i < data.length; i++) {
     const product = data[i];
@@ -25,6 +29,8 @@ const updateByProductId = async (product_id_list?: number[]) => {
       await getProductByNaverCatalogV2(product, i + 1, data.length);
       await wrapSlept(2000);
     }
+    await setGraph(product);
+    await setLastMonthLowPrice(product);
     l("timestamp", "cyan", new Date().toISOString());
   }
   l("[DONE]", "blue", "complete - all product price update");
@@ -34,3 +40,4 @@ const updateByProductId = async (product_id_list?: number[]) => {
 // updateByProductId(Array.from({ length: 100 }).map((a, i) => i + 1));
 updateByProductId();
 export { getAllProductIdType };
+// /product/price/low_price
