42662de74cb6f014ce96d39bf41623b26965e6df
gun-bro98 <one0374@naver.com>
2023-03-29 19:37:18 +0900

    crawling 관련 파일 backup 정리 및 알림 등록된 아이템스카우트 제품 크롤링하는 기능 추가

--- backup/unused_product_price_holyzone.ts
+++ backup/unused_product_price_holyzone.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { AuthorizationKey } from "./function/auth";
-import { NODE_API_URL, toComma } from "./function/common";
-import { l } from "./function/console";
-import { wrapSlept } from "./function/wrapSlept";
+import { AuthorizationKey } from "../function/auth";
+import { NODE_API_URL, toComma } from "../function/common";
+import { l } from "../function/console";
+import { wrapSlept } from "../function/wrapSlept";
 axios.defaults.headers.common["Authorization"] = `Bearer ${AuthorizationKey()}`;
 
 const updateHolyzone = async (product_id_list?: number[]) => {
@@ -11,18 +11,33 @@ const updateHolyzone = async (product_id_list?: number[]) => {
     `${NODE_API_URL}/crawling/product/holyzone/all`
   )
     .then((d) => d.data.data)
-    .catch((e) => l("Noti Err", "red", "성지존 알림 오류 /crawling/product/holyzone/all" + e.code));
+    .catch((e) =>
+      l(
+        "Noti Err",
+        "red",
+        "성지존 알림 오류 /crawling/product/holyzone/all" + e.code
+      )
+    );
 
   // 특정 제품만 가져오기 (없으면 전체 제품 대상)
-  if (product_id_list) data = data.filter((p) => product_id_list.includes(p.product_id));
+  if (product_id_list)
+    data = data.filter((p) => product_id_list.includes(p.product_id));
 
   for (let i = 0; i < data.length; i++) {
     const product = data[i];
     //#region 특정 제품 최저가 갱신 및 푸쉬보낼 user_id 가져오기
     const data2: { user_id: number; low_price: number }[] = await axios
-      .post(`${NODE_API_URL}/crawling/product/holyzone/notification`, { product_id: product.product_id })
+      .post(`${NODE_API_URL}/crawling/product/holyzone/notification`, {
+        product_id: product.product_id,
+      })
       .then((d) => d.data.data)
-      .catch((e) => l("Noti Err", "red", "성지존 알림 오류 /crawling/product/holyzone/notification" + e.code));
+      .catch((e) =>
+        l(
+          "Noti Err",
+          "red",
+          "성지존 알림 오류 /crawling/product/holyzone/notification" + e.code
+        )
+      );
 
     //#endregion
     //#region 제품 최저가 갱신시 유저에게 알림 보내기
@@ -33,14 +48,26 @@ const updateHolyzone = async (product_id_list?: number[]) => {
           .get(
             `${NODE_API_URL}/user/firebase/send/low_price?user_list=${userList}&title=야기야기&message=내가 관심을 보인 ${
               product.product_name
-            } 가격이 ${toComma(data2[0].low_price)}원으로 내려갔어요⬇️&link=/product/${product.product_id}`
+            } 가격이 ${toComma(
+              data2[0].low_price
+            )}원으로 내려갔어요⬇️&link=/product/${product.product_id}`
           )
-          .catch((e) => l("Noti Err", "red", "성지존 알림 오류 /user/firebase/send/low_price " + e.code));
+          .catch((e) =>
+            l(
+              "Noti Err",
+              "red",
+              "성지존 알림 오류 /user/firebase/send/low_price " + e.code
+            )
+          );
       }
       await wrapSlept(2000);
     }
     //#endregion
-    l("timestamp", "cyan", "product_id:" + product.product_id + ", " + new Date().toISOString());
+    l(
+      "timestamp",
+      "cyan",
+      "product_id:" + product.product_id + ", " + new Date().toISOString()
+    );
   }
   l("[DONE]", "blue", "complete - all product sending notification complete");
 };
---
+++ product_price_update_itemscout_subscription.ts
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { AuthorizationKey } from "./function/auth";
+import { NODE_API_URL } from "./function/common";
+import { l } from "./function/console";
+import { wrapSlept } from "./function/wrapSlept";
+import { getAllProductIdType } from "./product_price_update";
+import {
+  getNotificationItemscoutList,
+  setGraph,
+  setLastMonthLowPrice,
+  shuffle,
+} from "./function/product";
+import { getProductByItemscoutV2 } from "./function/updateByItemscoutV2";
+
+axios.defaults.headers.common["Authorization"] = `Bearer ${AuthorizationKey()}`;
+
+const updateByProductId = async (product_id_list?: number[]) => {
+  // (1) 키워드 가져올 제품아이디 전체 가져오기
+  let data: getAllProductIdType[] = await axios(
+    `${NODE_API_URL}/crawling/product/all`
+  ).then((d) => d.data.data);
+
+  // 특정 제품만 가져오기 (없으면 전체 제품 대상)
+  if (product_id_list)
+    data = data.filter((p) => product_id_list.includes(p.product_id));
+
+  //#region (2) 성지가격있는 제품아이디 모두 제외시키기
+  const exceptionList: number[] = await axios(
+    `${NODE_API_URL}/crawling/product/holyzone/all`
+  )
+    .then((d) => {
+      const data: { product_id: number; product_name: string }[] = d.data.data;
+      return data.map((p) => p.product_id);
+    })
+    .catch((e) => {
+      l(
+        "Noti Err",
+        "red",
+        "성지존 알림 오류 /crawling/product/holyzone/all" + e.code
+      );
+      return [];
+    });
+  data = data.filter((p) => !exceptionList.includes(p.product_id));
+  shuffle(data);
+
+  for (let i = 0; i < data.length; i++) {
+    const product = data[i];
+
+    if (product.type === "itemscout") {
+      await getProductByItemscoutV2(product, i + 1, data.length, true);
+      await wrapSlept(500);
+      await setGraph(product);
+      await setLastMonthLowPrice(product);
+      l("timestamp", "cyan", new Date().toISOString());
+    }
+  }
+  l("[DONE]", "blue", "complete - all product price update");
+};
+
+// updateByProductId([37327, 11191, 28560, 11311, 11775, 12166, 17697]);
+// updateByProductId(Array.from({ length: 100 }).map((a, i) => i + 1));
+const execute = async () => {
+  const notificationItemscoutList = await getNotificationItemscoutList();
+  updateByProductId(notificationItemscoutList);
+};
+execute();
