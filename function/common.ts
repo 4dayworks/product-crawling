f421db829970597ed003997b188ab7c93c6f760f
minsekim1 <tkarnrwl7862@naver.com>
2023-04-13 19:10:01 +0900

    아이허브 완료

--- function/common.ts
+++ function/common.ts
@@ -1,9 +1,8 @@
-const isLocalhost = false;
+const isLocalhost = false; ///
 const isTestDB = false;
 export const NODE_API_URL = isLocalhost
   ? "http://localhost:3001"
   : isTestDB
   ? "https://node3.yagiyagi.kr"
   : "https://node2.yagiyagi.kr";
-export const toComma = (number: string | number) =>
-  number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+export const toComma = (number: string | number) => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
