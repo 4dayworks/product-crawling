355e612dfacfa2a75a85265386b913da5c288115
minsekim1 <minseung@4dayworks.com>
2023-04-16 19:47:33 +0900

    backup

---
+++ function/iherb/updateByIherb.d.ts
@@ -0,0 +1,52 @@
+export type IherbProductPriceType = {
+  recommendedProducts: ProductDetailType[]; //2개씩 오는듯
+  originProduct: ProductDetailType;
+};
+export type ErrorType = {
+  errorMessage: "Product does not exist or is inactive. productId: 43826";
+  errorType: 0;
+};
+type ProductDetailType = {
+  id: number; //43724;
+  name: string; //"21st Century, 비오틴, 800mcg, 삼키기 쉬운 정제 110정";
+  partNumber: string; //"CEN22881";
+  url: string; //"https://kr.iherb.com/pr/21st-century-biotin-800-mcg-110-easy-swallow-tablets/43724";
+  brandCode: string; //"CEN";
+  brandName: string; //"21st Century";
+  primaryImageIndex: number; //39;
+  rating: number; //4.7;
+  ratingCount: number; //12977;
+  ratingUrl: string; //"https://kr.iherb.com/pr/21st-century-biotin-800-mcg-110-easy-swallow-tablets/43724#product-detail-reviews";
+  reviewUrl: string; //"https://kr.iherb.com/r/21st-century-biotin-800-mcg-110-easy-swallow-tablets/43724";
+  listPrice: string; //"₩2,840";
+  discountedPrice: string; //"₩2,840";
+  discountedPriceAmount: number; //2840;
+  hidePrice: boolean; //false;
+  productFlag: null;
+  discountType: number; // 0;
+  isInCartDiscount: boolean; //false;
+  salesDiscountPercentage: number; // 0;
+  specialDealInfo: null;
+  ratingStarsMap: number[]; // [Array]; 1,1,1,1,0.75 이렇게 오면 4.75점임
+  isInGroup: boolean; //false;
+  currencySymbol: string; //"₩";
+  isCurrencySymbolOnLeft: boolean; //true;
+};
+
+export type ProductType = {
+  id: number; // 112055;
+  displayName: string; // "이뮨 액티브, Protectis 캡슐, 2,000IU, 프로바이오틱 캡슐 60정";
+  isAvailableToPurchase: false;
+  partNumber: string; //"BGA-46041";
+  rootCategoryId: number; // 1855;
+  rootCategoryName: string; //"보충제";
+  url: string; //"https://kr.iherb.com/pr/biogaia-immune-active-protectis-capsules-2-000-iu-60-probiotic-capsules/112055";
+  urlName: string; //"biogaia-immune-active-protectis-capsules-2-000-iu-60-probiotic-capsules";
+  discountPrice: string; //"₩47,536";
+  listPrice: string; //"₩47,536";
+  brandCode: string; //"BGA";
+  brandName: string; //"BioGaia";
+  brandLogoUrl: string; //"https://s3.images-iherb.com/brand/logo/BGA/1642526326.jpg";
+  brandUrl: string; //"https://kr.iherb.com/c/biogaia";
+  primaryImageIndex: number; //12;
+};
--- iherb_update.ts
+++ iherb_update.ts
@@ -1,46 +1,70 @@
 import axios from "axios";
 import { AuthorizationKey } from "./function/auth";
-import { getBrandURLList, getMaxPageList, getProductData, getProductListData } from "./function/updateByIherb";
+import { getMaxPageList, getProductDescData, getProductListData, getProductPriceData } from "./function/updateByIherb";
 import { wrapSlept } from "./function/wrapSlept";
 import { l } from "./function/console";
+import { getBrandURLList } from "./function/iherb/brandList";
 
 axios.defaults.headers.common["Authorization"] = `Bearer ${AuthorizationKey()}`;
 
-const updateIherb = async (product_id_list?: number[]) => {
+// 판매처 집어넣고 최저가 반환하는 함수 -> 네이버/아이템스카우트 크롤링때 사용
+// const updateIherbByproductUrl =async () => {
+//   await getProductData({
+//     list_url: "https://kr.iherb.com/c/21st-century-health-care?p=3",
+//     product_url: "https://kr.iherb.com/pr/21st-century-wellify-men-s-50-multivitamin-multimineral-65-tablets/83157",
+//     brand: "21st Century (21st 센추리)",
+//   });
+// }
+
+// # (1)가격은 getProductPriceData 사용해서 가져오고(REST API 사용),
+// # (2)제품의 상세 데이터는 getProductDescData를 통해 가져옴(페이지 크롤링)
+// (2)는 iherb 데이터를 크롤링 봇으로 판단해 IP 막으므로 수동체크 반드시 필요. -> 주기적으로 돌리는 자동화 불가능
+const updateIherb = async (isInitProduct = false) => {
   // # (1) 브랜드 리스트 가져오기
   const brandURLList = getBrandURLList["active"];
 
   // # (2) 제품목록의 페이지별로 반복
+  // # 메인로직
   for (let brandIndex = 0; brandIndex < brandURLList.length; brandIndex++) {
+    //default: brandIndex = 0
     l(
       `[${brandIndex + 1}/${brandURLList.length + 1}]`,
       "blue",
       `next brand !, start_at: ${new Date().toISOString()}, brand-url: ${brandURLList[brandIndex]}`
     );
+
     const maxPage = await getMaxPageList(brandURLList[brandIndex]);
+    await wrapSlept(1000);
+
     if (maxPage.maxPage === null) return;
     for (let page = 1; page <= maxPage.maxPage; page++) {
+      //default: page = 1
       const productURLList = await getProductListData(`${maxPage.list_url}?p=${page}`);
       l(
         `[${page}/${maxPage.maxPage}]`,
         "green",
         `next page !, start_at: ${new Date().toISOString()}, page-url: ${maxPage.list_url}?p=${page}`
       );
+      await wrapSlept(1000);
+
       for (let i = 0; i < productURLList.length; i++) {
-        if (page === 1 && i < 46) continue;
+        //default: i = 0
+        // if (brandIndex == 2) continue;
+        // if (brandIndex == 4 && page == 3) continue;
+        // if (brandIndex == 4 && page == 7 && i < 18) continue;
         const product = productURLList[i];
         const start_at = new Date();
         l(
           `[${i + 1}/${productURLList.length}]`,
           "cyan",
-          `product_id: ${product.product_url.slice(
-            product.product_url.lastIndexOf("/") + 1,
-            product.product_url.length
-          )}, start_at: ${start_at.toISOString()}`
+          `product_id: ${product.product_url
+            .slice(product.product_url.lastIndexOf("/") + 1, product.product_url.length)
+            .padStart(6, " ")}, start_at: ${start_at.toISOString()}`
         );
-        await getProductData(product);
+        if (isInitProduct) await getProductDescData(product);
+        else await getProductPriceData(product);
         const end_at = new Date();
-        const wait_time = start_at.getTime() + 5000 - end_at.getTime();
+        const wait_time = start_at.getTime() + 200 - end_at.getTime();
         if (wait_time > 0) await wrapSlept(wait_time);
       }
     }
