162333fd1572192685c18396f0a63e3e51fe9c58
minsekim1 <tkarnrwl7862@naver.com>
2023-04-19 10:28:00 +0900

    특수문자 예외추가

--- function/iherb/headers.ts
+++ function/iherb/headers.ts
@@ -6,9 +6,9 @@ export const headers = () => {
     // "_px3=247e49052a0590e5ddeed16b4cb5339b99c63f3664c686370f9c55df0a16d695:VCTPcRRltzsvNlQj7bVj4yrZlVcvtVgEnFwfvVocZNScRDB7n/ITqTPEbo8hNeMjgi0waxBVNnKSylEynZxBAw==:1000:2Qo2sG0P7am4kNlGYuvy2kd2TRhnqaeTnrxyDzSpGPj7ZLy0Gcvd6IV4uMgUk7qDCY8OVsbVjkHN5Ov3DfZOQ0mu6j/oAOgpT7XTAWsmnFlmbJAvY7sAtNY0x1jQNz76V34VCdkyuk/e2rdDBdclYOYIgKaMd1hiVADLk8267vqO8fJXoGNOXn4+a90PFLtt17GxJZF8XAHAo9lwnDkyfg==",
     // "_px3=5bac1d1cd15a23149bbe7742040f9bdf62401b5023e778dcbe35ab51a440e5fd:H1f9C5T4T8LFfHJH2wzR0cc4CamWn+WmAxbPAsT6A59rQOWtKW6xLVYo2L52BlKb1t5aXXSu5gy5ylYWhRqjxw==:1000:9I2X2N1NNW8lM/d8mT3sN6+c76DiC55spXdztD8+8H7jd+H9d6DVIpHjOlaCKCIuhNoze+7/BOu4LInItrVdu5ab9huedZ+hgzL5vqh0Lz1EcaYl77sYzrPFsVye5nnYnsTxLKC3FQFU31TLmMp5/YsMJWL5Jt173Pd0hb5KhEvXpCABD5RTD/zDl8u2Mbksal8Z8PGE8tZdscHvxKujqw==",
     "_px3=741a8cadac13b4e37af01965b6d3d3cfffe96a7020b2fa3da85d9f955a1604cb:wdbrBkPT1HfF3kt9M545z8RpaEPfNqc3/XGuKiBh2AyOBTf2tMdEP7MmOWsJgDe9WhYnkwFC7QxCtZQ+Gld8FA==:1000:uVhapcKQ+LtCJBsMrOuwMolqRgwcCt5f+5GyXy7JZVqPlpCWjq/mDMSFCMRkkZhZ2c1L7Mb6Jb8mXkYIbP+CVzBf9+Gcq5a/zqPOQwrN0BFhisgvXhN1p+3nCIDVZdSWLV7RoIsdo9aaWGJvfBHT9C93H4X1dCD1Yryo+vySwlBY/Aq50R3JFDDzDztpZbliq6t7QCijGaQDKo+Octa6Xg==",
+    "_px3=a6644f354ac7885badd6295035c88109147ba3f4dfced5c71800231d3e82bb49:XMUGBo4i/tswVDiU2herOQG9H322WfBdIMXb2Eg4tr4ATW32dtIai3F1PzcWtlbdnfwf7YNhJdrsyp8iU80X3A==:1000:xe6mYdDWM1MS/L0kfZpLa0ztCSyMhKjHM4w5jcSr/3ScH45DDV0yQ0FM+9DUOpN+U3zKRVMfZqunQ+mTDButLA1FFefK5jU83uwtPIA6seph+E67jA6uKvZMg9IL59Z+cnTzoUWVlRFvpN2Gm+Mek6sa9/YkF4nPp/ISw2R4MSBfCY9xbrsV2xOUrpbwEGc+Myi0xvsAOCMA33q7OHiSXA==",
   ];
   const cookie = cookieList[i % cookieList.length];
-  // console.log("axios index:", i);
   i++;
   return {
     headers: {
