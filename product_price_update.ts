1230c911205440f5e16dcf3adb288b1c445c5f19
minsekim1 <minseung@4dayworks.com>
2023-04-29 15:17:29 +0900

    dev 모드 해제

--- product_price_update.ts
+++ product_price_update.ts
@@ -70,4 +70,4 @@ const updateByProductId = async (product_id_list?: number[]) => {
 
 // updateByProductId([37327, 11191, 28560, 11311, 11775, 12166, 17697]);
 // updateByProductId(Array.from({ length: 100 }).map((a, i) => i + 1));
-updateByProductId([135]);
+updateByProductId();
