96462c7e803dd246304e69b568f97d26e96e6b3c
minsekim1 <tkarnrwl7862@naver.com>
2023-05-16 12:00:26 +0900

    쿠팡 판매처 추가

---
+++ function/coupang/getCoupangStoreData.ts
@@ -0,0 +1,109 @@
+import axios from "axios";
+import { getAllProductIdType } from "../../product_price_update";
+import { l } from "../console";
+import { NODE_API_URL } from "../common";
+import { AuthorizationKey } from "../auth";
+
+axios.defaults.headers.common["Authorization"] = `Bearer ${AuthorizationKey()}`;
+export const getCoupangStoreData = async ({
+  product_id,
+  product_name,
+}: // coupang_itemscout_keyword,
+// coupang_itemscout_keyword_id,
+
+// coupang_exception_keyword_list,
+// coupang_allow_tag,
+// coupang_require_keyword_list,
+getAllProductIdType) => {
+  // 개요
+  // 1. 키워드/키워드id DB에서 가져오기
+  // 2. 키워드id 없을경우 itemscout 에서 keyword_id 가져오기
+  //  2-1. 야기야기 DB에 키워드 저장
+  // 3. 아이템스카우트에서 판매처가져와서 필터링하기, tag, exception_list, require_list 활용
+  //  3-1. exception_list, require_list, rocketType(coupang_allow_tag) 로 필터링하기
+  // 4. 필터링된 판매처 함수에서 반환하기
+
+  // 1. 키워드/키워드id DB에서 가져오기
+  const {
+    coupang_itemscout_keyword_id,
+    coupang_itemscout_keyword,
+    coupang_require_keyword_list,
+    coupang_exception_keyword_list,
+    coupang_allow_tag,
+  }: CoupangDataType = await axios
+    .get(`${NODE_API_URL}/crawling/product/coupang/keyword?product_id=${product_id}`)
+    .then((d) => d.data.data);
+
+  // 2. 키워드id 없을경우 itemscout 에서 keyword_id 가져오기
+  let keyword_id = coupang_itemscout_keyword_id;
+  const keyword = coupang_itemscout_keyword ? coupang_itemscout_keyword : product_name;
+  if (!coupang_itemscout_keyword_id) {
+    const url = `https://api.itemscout.io/api/keyword`;
+    const itemscout_keyword_id = await axios.post(url, { keyword }, { headers }).then((d) => d.data.data);
+    keyword_id = itemscout_keyword_id;
+
+    // 2-1. 야기야기 DB에 키워드 저장
+    await axios.post(`${NODE_API_URL}/crawling/product/coupang/keyword`, {
+      product_id,
+      keyword_id: itemscout_keyword_id,
+      keyword,
+    });
+  }
+
+  // 에러처리
+  if (!keyword_id) {
+    l("Err", "red", `[coupang] No keyword_id product_id:${product_id}`);
+    return [];
+  }
+
+  // 3. 아이템스카우트에서 판매처가져와서 필터링하기, tag, exception_list, require_list 활용
+  let storeList: ItemscoutCoupangStoreType[] = await axios
+    .get(`https://api.itemscout.io/api/v2/keyword/products?kid=${keyword_id}&type=coupang`, { headers })
+    .then((d) => d.data.data.productListResult)
+    .catch(() => []);
+
+  // 3-1. exception_list, require_list, rocketType(coupang_allow_tag) 로 필터링하기
+
+  const exception_list = coupang_exception_keyword_list
+    ? coupang_exception_keyword_list.split(",").map((k) => k.trim())
+    : [];
+  const require_list = coupang_require_keyword_list ? coupang_require_keyword_list.split(",").map((k) => k.trim()) : [];
+  const allow_tag = coupang_allow_tag ? coupang_allow_tag.split(",").map((k) => k.trim()) : [];
+
+  storeList = storeList
+    .filter(
+      (s) =>
+        require_list.map((r) => s.title.includes(r)).filter((b) => b === false).length === 0 &&
+        exception_list.map((r) => s.title.includes(r)).filter((b) => b === true).length === 0 &&
+        s.rocketType &&
+        allow_tag.includes(s.rocketType) &&
+        !s.isAd
+    )
+    .slice(0, 2);
+
+  // 4. 필터링된 판매처 함수에서 반환하기
+  return storeList;
+};
+
+const headers = { "Accept-Encoding": "deflate, br" };
+
+type CoupangDataType = {
+  coupang_itemscout_keyword_id: string | null;
+  coupang_itemscout_keyword: string | null;
+  coupang_require_keyword_list: string | null;
+  coupang_exception_keyword_list: string | null;
+  coupang_allow_tag: string | null;
+};
+export type ItemscoutCoupangStoreType = {
+  seq: number; //1,
+  productId: string; //"205187263";
+  title: string; //"Evlution Nutrition L-카르니틴 500mg 캡슐, 120개입, 1개";
+  price: number; //17200,
+  image: string; //"https://thumbnail10.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/images/2019/04/03/17/6/5958c12d-e34f-4393-83a4-29f49f67ae62.jpg";
+  link: string; //"https://www.coupang.com/vp/products/205187263?itemId=604043537&vendorItemId=4583175650";
+  isAd: boolean;
+  isRocket: boolean;
+  rocketType: "로켓직구" | "로켓배송" | "제트배송" | "로켓프레시" | null; //"로켓직구";
+  reviewScore: number; //4.5;
+  reviewCount: number; //24;
+};
--- product_price_update_naver.ts
+++ product_price_update_naver.ts
@@ -6,32 +6,24 @@ import { getProductByNaverCatalogV2 } from "./function/getProductByNaverCatalogV
 import { wrapSlept } from "./function/wrapSlept";
 import { getAllProductIdType } from "./product_price_update.d";
 import { setGraph, setLastMonthLowPrice, shuffle } from "./function/product";
+import { getCoupangStoreData } from "./function/coupang/getCoupangStoreData";
 
 axios.defaults.headers.common["Authorization"] = `Bearer ${AuthorizationKey()}`;
 
 const updateByProductId = async (product_id_list?: number[]) => {
   // (1) 키워드 가져올 제품아이디 전체 가져오기
-  let data: getAllProductIdType[] = await axios(
-    `${NODE_API_URL}/v2/crawling/product/all`
-  ).then((d) => d.data.data);
+  let data: getAllProductIdType[] = await axios(`${NODE_API_URL}/v3/crawling/product/all`).then((d) => d.data.data);
   // 특정 제품만 가져오기 (없으면 전체 제품 대상)
-  if (product_id_list)
-    data = data.filter((p) => product_id_list.includes(p.product_id));
+  if (product_id_list) data = data.filter((p) => product_id_list.includes(p.product_id));
 
   //#region (2) 성지가격있는 제품아이디 모두 제외시키기
-  const exceptionList: number[] = await axios(
-    `${NODE_API_URL}/crawling/product/holyzone/all`
-  )
+  const exceptionList: number[] = await axios(`${NODE_API_URL}/crawling/product/holyzone/all`)
     .then((d) => {
       const data: { product_id: number; product_name: string }[] = d.data.data;
       return data.map((p) => p.product_id);
     })
     .catch((e) => {
-      l(
-        "Noti Err",
-        "red",
-        "성지존 알림 오류 /crawling/product/holyzone/all" + e.code
-      );
+      l("Noti Err", "red", "성지존 알림 오류 /crawling/product/holyzone/all" + e.code);
       return [];
     });
   data = data.filter((p) => !exceptionList.includes(p.product_id));
@@ -40,18 +32,19 @@ const updateByProductId = async (product_id_list?: number[]) => {
 
   for (let i = 0; i < data.length; i++) {
     const product = data[i];
+    const coupangStoreList = await getCoupangStoreData(product);
 
     if (product.type === "naver" && product.naver_catalog_link) {
-      await getProductByNaverCatalogV2(product, i + 1, data.length);
+      await getProductByNaverCatalogV2(product, i + 1, data.length, coupangStoreList);
       await wrapSlept(2000);
       await setGraph(product);
       await setLastMonthLowPrice(product);
     }
-    l("timestamp", "cyan", new Date().toISOString());
+    l("timestamp", "blue", new Date().toISOString());
   }
   l("[DONE]", "blue", "complete - all product price update");
 };
 
 // updateByProductId([37327, 11191, 28560, 11311, 11775, 12166, 17697]);
 // updateByProductId(Array.from({ length: 100 }).map((a, i) => i + 1));
-updateByProductId([23356, 87, 250, 57804, 144, 3368, 57791, 71]);
+updateByProductId();
