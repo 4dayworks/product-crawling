e77e6b7f8f15a8b0178ee8eb191f47917ee3e396
gunhyung <one0374@naver.com>
2023-05-19 14:57:51 +0900

    도메인 node -> node2

--- function/common.ts
+++ function/common.ts
@@ -4,6 +4,6 @@ export const NODE_API_URL = isLocalhost
   ? "http://localhost:3001"
   : isTestDB
   ? "https://node3.yagiyagi.kr"
-  : "https://node.yagiyagi.kr";
+  : "https://node2.yagiyagi.kr";
 export const toComma = (number: string | number) =>
   number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
