08e6a77fad761146a9d75a68f99be8e601e95a3c
gun-bro98 <one0374@naver.com>
2023-05-23 18:34:50 +0900

    폴더 정리

--- product_price_update.ts
+++ product_price_update.ts
@@ -4,6 +4,4 @@ import { updateByProductId } from "./all_update";
 axios.defaults.headers.common["Authorization"] = `Bearer ${AuthorizationKey()}`;
 
 // updateByProductId({page: 0, size: 3000, product_id_list: [12414,121213]});
-updateByProductId({
-  product_id_list: Array.from({ length: 100 }).map((_, i) => i + 100),
-});
+updateByProductId({});
