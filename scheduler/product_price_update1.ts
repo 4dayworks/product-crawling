0f079cefec0b8e1d79164a40f2152c8b8a5550d5
minsekim1 <tkarnrwl7862@naver.com>
2023-05-23 16:18:08 +0900

    schedulere to ./

--- scheduler/product_price_update1.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import { AuthorizationKey } from "../function/auth";
-import { updateByProductId } from "../all_update";
-axios.defaults.headers.common["Authorization"] = `Bearer ${AuthorizationKey()}`;
-
-// updateByProductId({page: 0, size: 3000, product_id_list: [12414,121213]});
-updateByProductId({ page: 1, size: 5000 });
