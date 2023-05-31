d9d0d22447b61a0b067506dbe64762e8bd65888c
minsekim1 <tkarnrwl7862@naver.com>
2023-05-31 11:11:56 +0900

    dev 모드해제

--- backup/unused_product_price_update_naver_schedular.ts
+++ backup/unused_product_price_update_naver_schedular.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { AuthorizationKey } from "../function/auth";
 import { NODE_API_URL } from "../function/common";
 import { l } from "../function/console";
-import { getProductByNaverCatalogV2 } from "../function/getProductByNaverCatalogV2";
+import { getNaverCatalogStoreListV2 } from "../function/getNaverCatalogStoreListV2";
 import { wrapSlept } from "../function/wrapSlept";
 import { getAllProductIdType } from "../function/product_price_update";
 axios.defaults.headers.common["Authorization"] = `Bearer ${AuthorizationKey()}`;
@@ -20,13 +20,10 @@ axios.defaults.headers.common["Authorization"] = `Bearer ${AuthorizationKey()}`;
 
 const updateByProductId = async (product_id_list?: number[]) => {
   // (1) 키워드 가져올 제품아이디 전체 가져오기
-  let data: getAllProductIdType[] = await axios(
-    `${NODE_API_URL}/crawling/product/all`
-  ).then((d) => d.data.data);
+  let data: getAllProductIdType[] = await axios(`${NODE_API_URL}/crawling/product/all`).then((d) => d.data.data);
 
   // 특정 제품만 가져오기 (없으면 전체 제품 대상)
-  if (product_id_list)
-    data = data.filter((p) => product_id_list.includes(p.product_id));
+  if (product_id_list) data = data.filter((p) => product_id_list.includes(p.product_id));
 
   for (let i = 0; i < data.length; i++) {
     const product = data[i];
@@ -36,7 +33,7 @@ const updateByProductId = async (product_id_list?: number[]) => {
     //   await wrapSlept(300);
     // } else
     if (product.type === "naver" && product.naver_catalog_link) {
-      await getProductByNaverCatalogV2(product, i + 1, data.length, true);
+      await getNaverCatalogStoreListV2(product, i + 1, data.length, true);
       // await insertGraph(product);
       await wrapSlept(2000);
     }
