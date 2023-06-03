bbea716c8cdf509dce36b34365807e98ea8efca4
gunhyung <one0374@naver.com>
2023-06-03 11:20:32 +0900

    url 변경

--- function/common.ts
+++ function/common.ts
@@ -1,5 +1,5 @@
 export const isLocalhost = false; /// default: false
-const isTestDB = true;
+const isTestDB = false;
 export const NODE_API_URL = isLocalhost
   ? "http://localhost:3001"
   : isTestDB
