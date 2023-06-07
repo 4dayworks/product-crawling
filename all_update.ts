9c6e96947dc3570283b76fe39934fbef46ab7041
minsekim1 <tkarnrwl7862@naver.com>
2023-06-07 10:07:16 +0900

    오류발생시 대기5분->10분

--- all_update.ts
+++ all_update.ts
@@ -11,7 +11,7 @@ type updateByProductIdType = {
   product_id_list?: number[];
 };
 
-export const updateByProductId = async ({ page = 0, size = 100000, product_id_list }: updateByProductIdType) => {
+export const updateByProductId = async ({ page = 0, size = 1000000, product_id_list }: updateByProductIdType) => {
   // (1) 키워드 가져올 제품아이디 전체 가져오기
   let list: getAllProductIdType[] = await axios(
     `${NODE_API_URL}/v4/crawling/product/all?page=${page}&size=${size}`
@@ -27,34 +27,34 @@ export const updateByProductId = async ({ page = 0, size = 100000, product_id_li
   // 배열 섞기
   // if (!product_id_list) list = shuffle(list);
 
-  // //#region itemscout / naver type에 따라서 번갈아가며 한번씩 배열에 넣기
-  // const grouped = groupBy(list, "type");
-  // const combinedList: getAllProductIdType[] = [];
-  // const types = ["naver", "itemscout"]; // 순서에 따라 번갈아가며 그룹화
-  // const maxLength = Math.max(
-  //   grouped.naver ? grouped.naver.length : 0,
-  //   grouped.itemscout ? grouped.itemscout.length : 0
-  // );
-  // for (let i = 0; i < maxLength; i++) {
-  //   types.forEach((type) => {
-  //     if (grouped[type] && i < grouped[type].length) combinedList.push(grouped[type][i]);
-  //   });
-  // }
-  // list = combinedList;
+  //#region itemscout / naver type에 따라서 번갈아가며 한번씩 배열에 넣기
+  const grouped = groupBy(list, "type");
+  const combinedList: getAllProductIdType[] = [];
+  const types = ["naver", "itemscout"]; // 순서에 따라 번갈아가며 그룹화
+  const maxLength = Math.max(
+    grouped.naver ? grouped.naver.length : 0,
+    grouped.itemscout ? grouped.itemscout.length : 0
+  );
+  for (let i = 0; i < maxLength; i++) {
+    types.forEach((type) => {
+      if (grouped[type] && i < grouped[type].length) combinedList.push(grouped[type][i]);
+    });
+  }
+  list = combinedList;
 
   //#endregion
 
   // 70000번 이상으로 거르기
-  // list = list.filter((s) => s.product_id <= 70000);
+  list = list.filter((s) => s.product_id <= 70000);
   let chance = 3; //다시 시도할 기회
-  for (let i = 12000; i < list.length; i++) {
+  for (let i = 0; i < list.length; i++) {
     if (list.length > i) {
       const result = await setData(list[i], i, list.length);
       // l("[result]", "magenta", JSON.stringify(result));
       if (!result) {
         if (chance > 0) {
-          // 문제 생겼을시 5분 또는 20초 대기 후 다음 재시도
-          await wrapSlept(chance === 1 ? 300000 : 20000);
+          // 문제 생겼을시 10분 또는 20초 대기 후 다음 재시도
+          await wrapSlept(chance === 1 ? 600000 : 20000);
           chance--;
           if (chance === 1) i--;
           continue;
