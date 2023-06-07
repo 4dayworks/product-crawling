2ba27f9557349b827c64ce4be639e7d0f03bad3d
minsekim1 <tkarnrwl7862@naver.com>
2023-06-07 16:50:37 +0900

    .

--- product_price_update.ts
+++ product_price_update.ts
@@ -7,5 +7,5 @@ axios.defaults.headers.common["Authorization"] = `Bearer ${AuthorizationKey()}`;
 updateByProductId({
   // page: 0,
   // size: 6000,
-  product_id_list: [91656],
+  product_id_list: [90257],
 });
