e0106204f3ae92f2537167e7a3879bbcc0e7decf
minsekim1 <tkarnrwl7862@naver.com>
2023-06-08 12:08:53 +0900

    naver count <-> rating 오류 수정

--- function/getItemscoutStoreListV2.ts
+++ function/getItemscoutStoreListV2.ts
@@ -12,15 +12,11 @@ export const getItemscoutStoreListV2 = (product: getAllProductIdType) =>
       //#region (2) 키워드 가져오기 & 있는지 확인하고 야기 DB에 반영하기
       // GET /product/keyword/id
       let keyword_id = originData.keyword_id ? originData.keyword_id : null;
-      let keyword = originData.keyword
-        ? originData.keyword
-        : originData.product_name;
+      let keyword = originData.keyword ? originData.keyword : originData.product_name;
       //  (2-1). keyword가 있고 keyword_id가 없으면 해당 키워드로 검색하고 keyword_id update하기
       const url = `https://api.itemscout.io/api/keyword`;
       if (!keyword_id) {
-        const itemscout_keyword_id = await axios
-          .post(url, { keyword }, { headers })
-          .then((d) => d.data.data);
+        const itemscout_keyword_id = await axios.post(url, { keyword }, { headers }).then((d) => d.data.data);
         keyword_id = itemscout_keyword_id;
       }
       //#endregion
@@ -44,20 +40,14 @@ export const getItemscoutStoreListV2 = (product: getAllProductIdType) =>
       // 리스트 없을 경우 제품명으로 다시 검색
       if (productListResult.length === 0) {
         keyword = originData.product_name;
-        const itemscout_keyword_id = await axios
-          .post(url, { keyword }, { headers })
-          .then((d) => d.data.data);
+        const itemscout_keyword_id = await axios.post(url, { keyword }, { headers }).then((d) => d.data.data);
         keyword_id = itemscout_keyword_id;
         productListResult = await axios(
           `https://api.itemscout.io/api/v2/keyword/products?kid=${keyword_id}&type=total`,
           { headers }
         ).then((d) => {
           if (!d.data.data.productListResult) return [];
-          const list = filterArray(
-            d.data.data.productListResult,
-            product,
-            originData
-          );
+          const list = filterArray(d.data.data.productListResult, product, originData);
 
           return list;
         });
@@ -74,7 +64,7 @@ export const getItemscoutStoreListV2 = (product: getAllProductIdType) =>
           store_price: item.price, // 25900;
           store_category: item.category, // "식품>건강식품>영양제>기타건강보조식품";
           store_review_count: item.reviewCount, // 19;
-          store_review_score: item.reviewCount, //5;
+          store_review_score: item.reviewScore, //5;
           store_link: item.mobileProductUrl || item.pcProductUrl || item.link,
           store_is_oversea: item.isOversea,
           store_is_navershop: item.isNaverShop,
