7823e0fef673b3959ca74524bea9c7af02d19910
minsekim1 <tkarnrwl7862@naver.com>
2023-06-20 14:58:35 +0900

    GCP metatag : name에서 가져와서 startIndex 활용추ㄱ

--- product_price_update_always.ts
+++ product_price_update_always.ts
@@ -6,18 +6,59 @@ axios.defaults.headers.common["Authorization"] = `Bearer ${AuthorizationKey()}`;
 // GCP 자동 인스턴스 재생성 후
 // 해당 인덱스로 이동을 위해 메타데이터를 넣어줄떄 사용합니다.
 const execute = async () => {
-  const response = await axios
+  const startIndex = await axios
     .get("http://metadata.google.internal/computeMetadata/v1/instance/?recursive=true", {
       headers: {
         "Metadata-Flavor": "Google",
       },
     })
-    .then((d) => d.data)
-    .catch(() => null);
-  console.log(response);
-  const startIndex = response ? response : 0;
+    .then((d: { data: ResponseType }) => {
+      const regex = /start-index-(\d+)/;
+      const startIndex = d.data.name.match(regex);
+      return !isNaN(Number(startIndex)) ? Number(startIndex) : undefined;
+    })
+    .catch(() => undefined);
   while (true) {
     await updateByProductId({ startIndex });
   }
 };
 execute();
+
+type ResponseType = {
+  attributes: {
+    "ssh-keys": string;
+  };
+  cpuPlatform: string;
+  description: string;
+  disks: [{ deviceName: "persistent-disk-0"; index: 0; interface: "SCSI"; mode: "READ_WRITE"; type: "PERSISTENT" }];
+  guestAttributes: {};
+  hostname: string;
+  id: number;
+  image: string;
+  licenses: [{ id: string }];
+  machineType: string;
+  maintenanceEvent: string;
+  name: string; //"product-crawling-202306201451-cd0dd723-start-index-24";
+  networkInterfaces: [
+    {
+      accessConfigs: [{ externalIp: string; type: "ONE_TO_ONE_NAT" }];
+      dnsServers: [string];
+      forwardedIps: [];
+      gateway: string;
+      ip: string;
+      ipAliases: [];
+      mac: string;
+      mtu: 1460;
+      network: string;
+      subnetmask: string;
+      targetInstanceIps: [];
+    }
+  ];
+  preempted: string;
+  remainingCpuTime: -1;
+  scheduling: { automaticRestart: "TRUE"; onHostMaintenance: "MIGRATE"; preemptible: "FALSE" };
+  serviceAccounts: {};
+  tags: [];
+  virtualClock: { driftToken: "0" };
+  zone: string;
+};
