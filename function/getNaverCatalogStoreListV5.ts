9de83e8a87601dda2cd0e9c46112fb80bc0d86e7
minsekim <tkarnrwl7862@naver.com>
2023-07-04 23:49:18 +0900

    네이버 모바일 링크도 사진 가져오기 추가

--- function/getNaverCatalogStoreListV5.ts
+++ function/getNaverCatalogStoreListV5.ts
@@ -32,7 +32,15 @@ export const getNaverCatalogStoreListV5 = ({ product_id, naver_catalog_url }: ge
 
         const origin_product_name =
           "(네이버 쇼핑) " + $(`#__next > div > div > div > div > div > h2`).text().replace("제품속성", "");
-        const product_image = $(`#__next > div > div > div > div > div > img`).attr("src") || null;
+        let product_image = $(`#__next > div > div > div > div > div > img`).attr("src") || null;
+
+        // 모바일 버전으로 가져오기
+        const text =
+          "background-image:url(https://shopping-phinf.pstatic.net/main_2346274/23462749491.20210802142534.jpg?type=w640)";
+        const urlPattern = /url\((https?:\/\/[^\)]+)\)/;
+        const match = text.match(urlPattern);
+        if (!product_image && match && match[1]) product_image = match[1];
+
         const regex = /[^0-9]/g;
         const reviewCount = Number($(`#section-review > div > div > h3`).text().replace(regex, ""));
         try {
