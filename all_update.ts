6a5d651e0d4ed9530d918ffec33c2fc3dd67ec27
minsekim <tkarnrwl7862@naver.com>
2023-07-08 11:07:51 +0900

    start index chagned

--- all_update.ts
+++ all_update.ts
@@ -59,7 +59,7 @@ export const updateByProductId = async ({
   // if (!productSelectedList) productIdListAll = shuffle(productIdListAll);
 
   let chance = 3; //다시 시도할 기회
-  for (let i = 410; i < productIdListAll.length; i++) {
+  for (let i = 610; i < productIdListAll.length; i++) {
     // if (isInit && instanceData?.startIndex != undefined && instanceData.startIndex > 0) {
     //   i = instanceData.startIndex;
     //   isInit = false;
