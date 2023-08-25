502d102a21fa82c5fdad487aa66923b0b18ca6fa
minsekim <tkarnrwl7862@naver.com>
2023-08-25 10:51:54 +0900

    version up

--- all_update.ts
+++ all_update.ts
@@ -141,9 +141,7 @@ export const updateByProductId = async ({
                 .catch((err) => l("Err", "red", "Slack Send Message Error"));
               await axios
                 .get(
-                  `http://34.22.78.170:3001/gcp/restart?instance_name=${instanceData.instance_name}&start_index=${
-                    i - 1
-                  }`
+                  `http://34.22.78.170:3001/gcp/restart?instance_name=product-crawling-202308230300-e2-start-index-12568`
                 )
                 .then((res) => res.data.data)
                 .catch((err) => l("Err", "red", "Slack Send Message Error"));
