c719166f360a52fa90438c9e648a397a825186af
minsekim <tkarnrwl7862@naver.com>
2023-08-31 14:18:50 +0900

    쿠팡 압축 header 제거

--- function/getCoupangStoreListV5.ts
+++ function/getCoupangStoreListV5.ts
@@ -9,7 +9,7 @@ export const getCoupangStoreListV5 = async ({ coupang_keyword }: getProductTypeV
   const getHeaders = () => {
     return {
       Accept: "*/*",
-      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Encoding": "deflate, br",
       "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
