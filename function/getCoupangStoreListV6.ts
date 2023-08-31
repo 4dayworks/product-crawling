3f053275ffd4ebfeac77f8cf2fc27dc09094ace4
minsekim <tkarnrwl7862@naver.com>
2023-08-31 10:14:44 +0900

    coupang test

--- function/getCoupangStoreListV6.ts
+++ function/getCoupangStoreListV6.ts
@@ -19,7 +19,7 @@ export const getCoupangStoreListV6 = async ({ coupang_keyword }: getProductTypeV
     const url = `https://www.coupang.com/np/search?rocketAll=true&q=${encodeURIComponent(coupang_keyword)}`;
     await driver.get(url);
 
-    const productElements = await driver.findElements(By.id("productList"));
+    const productElements = await driver.findElements(By.id("#productList"));
     const storeList: StoreTypeV5[] = [];
 
     console.log({ url, productElements });
