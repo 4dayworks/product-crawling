296e9ca241dd7d62e1ab977902f90fb30a2dc231
minsekim <tkarnrwl7862@naver.com>
2023-08-31 11:23:38 +0900

    coupang test

--- function/getCoupangStoreListV6.ts
+++ function/getCoupangStoreListV6.ts
@@ -129,83 +129,84 @@ export const getCoupangStoreListV6 = async ({ coupang_keyword }: getProductTypeV
       console.log({ bodyContent });
     }
 
-    const productElements = await driver.findElements(By.css("a.search-product-link"));
-    const storeList: StoreTypeV5[] = [];
+    // const productElements = await driver.findElements(By.css("a.search-product-link"));
+    // const storeList: StoreTypeV5[] = [];
 
-    for (let element of productElements) {
-      const store_product_name = await element.findElement(By.css("dl > dd > div > div.name")).getText();
+    // for (let element of productElements) {
+    //   const store_product_name = await element.findElement(By.css("dl > dd > div > div.name")).getText();
 
-      const store_product_image_data_src =
-        "https:" + (await element.findElement(By.css("dl > dt > img")).getAttribute("data-img-src"));
-      const store_product_image_src =
-        "https:" + (await element.findElement(By.css("dl > dt > img")).getAttribute("src"));
+    //   const store_product_image_data_src =
+    //     "https:" + (await element.findElement(By.css("dl > dt > img")).getAttribute("data-img-src"));
+    //   const store_product_image_src =
+    //     "https:" + (await element.findElement(By.css("dl > dt > img")).getAttribute("src"));
 
-      const store_product_image =
-        store_product_image_src.includes("undefined") || store_product_image_src.includes("blank1x1")
-          ? store_product_image_data_src
-          : store_product_image_src;
+    //   const store_product_image =
+    //     store_product_image_src.includes("undefined") || store_product_image_src.includes("blank1x1")
+    //       ? store_product_image_data_src
+    //       : store_product_image_src;
 
-      const store_link = "https://www.coupang.com" + (await element.getAttribute("href"));
+    //   const store_link = "https://www.coupang.com" + (await element.getAttribute("href"));
 
-      const store_price = Number(
-        (await element.findElement(By.css("dl > dd > div > div.price-area > div > div.price > em > strong")).getText())
-          .trim()
-          .replace(/,/g, "")
-      );
+    //   const store_price = Number(
+    //     (await element.findElement(By.css("dl > dd > div > div.price-area > div > div.price > em > strong")).getText())
+    //       .trim()
+    //       .replace(/,/g, "")
+    //   );
 
-      const typeSrc = await element
-        .findElement(By.css("dl > dd > div > div.price-area > div > div.price > em > span > img"))
-        .getAttribute("src");
-      const outOfStock = await element
-        .findElement(By.css("dl > dd > div > div.price-area > div.out-of-stock"))
-        .getText();
+    //   const typeSrc = await element
+    //     .findElement(By.css("dl > dd > div > div.price-area > div > div.price > em > span > img"))
+    //     .getAttribute("src");
+    //   const outOfStock = await element
+    //     .findElement(By.css("dl > dd > div > div.price-area > div.out-of-stock"))
+    //     .getText();
 
-      const type = !typeSrc
-        ? null
-        : typeSrc.includes("merchant")
-        ? "제트배송"
-        : typeSrc.includes("fresh")
-        ? "로켓프레시"
-        : typeSrc.includes("global")
-        ? "로켓직구"
-        : typeSrc.includes("rocket")
-        ? "로켓배송"
-        : null;
+    //   const type = !typeSrc
+    //     ? null
+    //     : typeSrc.includes("merchant")
+    //     ? "제트배송"
+    //     : typeSrc.includes("fresh")
+    //     ? "로켓프레시"
+    //     : typeSrc.includes("global")
+    //     ? "로켓직구"
+    //     : typeSrc.includes("rocket")
+    //     ? "로켓배송"
+    //     : null;
 
-      const store_review_score = Number(
-        await element.findElement(By.css("dl > dd > div > div.other-info > div > span.star > em")).getText()
-      );
+    //   const store_review_score = Number(
+    //     await element.findElement(By.css("dl > dd > div > div.other-info > div > span.star > em")).getText()
+    //   );
 
-      const store_review_count = Number(
-        (await element.findElement(By.css("dl > dd > div > div.other-info > div > span.rating-total-count")).getText())
-          .trim()
-          .replace(/\(|\)/g, "")
-      );
+    //   const store_review_count = Number(
+    //     (await element.findElement(By.css("dl > dd > div > div.other-info > div > span.rating-total-count")).getText())
+    //       .trim()
+    //       .replace(/\(|\)/g, "")
+    //   );
 
-      const is_ad = (await element.findElement(By.css("dl > dd > div > span > span.ad-badge-text")).getText()) === "AD";
+    //   const is_ad = (await element.findElement(By.css("dl > dd > div > span > span.ad-badge-text")).getText()) === "AD";
 
-      if (!type || is_ad || !store_product_image || outOfStock === "일시품절") continue;
+    //   if (!type || is_ad || !store_product_image || outOfStock === "일시품절") continue;
 
-      const data = {
-        yagi_keyword: coupang_keyword,
-        origin_product_name: store_product_name,
-        product_image: store_product_image,
-        mall_image: null,
-        price: store_price,
-        delivery: 0,
-        store_name: type,
-        category: null,
-        review_count: store_review_count,
-        review_score: store_review_score,
-        is_naver_shop: false,
-        is_oversea: type === "로켓직구",
-        store_link,
-      };
+    //   const data = {
+    //     yagi_keyword: coupang_keyword,
+    //     origin_product_name: store_product_name,
+    //     product_image: store_product_image,
+    //     mall_image: null,
+    //     price: store_price,
+    //     delivery: 0,
+    //     store_name: type,
+    //     category: null,
+    //     review_count: store_review_count,
+    //     review_score: store_review_score,
+    //     is_naver_shop: false,
+    //     is_oversea: type === "로켓직구",
+    //     store_link,
+    //   };
 
-      storeList.push(data);
-    }
+    //   storeList.push(data);
+    // }
 
-    return storeList;
+    // return storeList;
+    return [];
   } catch (error) {
     console.error("Error in web scraping:", error);
     throw error;
