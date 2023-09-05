5618a97564b585d2227834d30544a1af651b2338
minsekim1 <tkarnrwl7862@naver.com>
2023-09-05 11:38:38 +0900

    네이버 제품상세 이미지 자동화 완료

--- script/get_product_image.sh
+++ script/get_product_image.sh
@@ -2,19 +2,13 @@
 
 LOG_FILE="/Users/minsekim/projects/Yagiyagi/product-crawling/script/get_product_image.log"
 
-echo "[$(date)] Script started" >> $LOG_FILE
+echo "[$(date)] Script started" > $LOG_FILE
 
-# NVM 환경 설정
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-# 원하는 Node 버전 활성화
-nvm use v16.20.1
+# Node.js와 npm의 경로 추가
+export PATH="/Users/minsekim/.nvm/versions/node/v16.20.1/bin:$PATH"
 
 cd /Users/minsekim/projects/Yagiyagi/product-crawling
-
-/Users/minsekim/.yarn/bin/yarn update:product-image >> $LOG_FILE 2>&1
+npm run update:product-image >> $LOG_FILE 2>&1
 
 if [ $? -eq 0 ]; then
     echo "[$(date)] Script completed successfully" >> $LOG_FILE
