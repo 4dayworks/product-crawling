cf45ebcbc4a5470994739efe7a812831b5380233
minsekim1 <tkarnrwl7862@naver.com>
2023-09-05 11:23:46 +0900

    npm run patch

--- script/get_product_image.sh
+++ script/get_product_image.sh
@@ -1,1 +1,23 @@
-yarn update:product-image
+#!/bin/bash
+
+LOG_FILE="/Users/minsekim/projects/Yagiyagi/product-crawling/script/get_product_image.log"
+
+echo "[$(date)] Script started" >> $LOG_FILE
+
+# NVM 환경 설정
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# 원하는 Node 버전 활성화
+nvm use v16.20.1
+
+cd /Users/minsekim/projects/Yagiyagi/product-crawling
+
+/Users/minsekim/.yarn/bin/yarn update:product-image >> $LOG_FILE 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "[$(date)] Script completed successfully" >> $LOG_FILE
+else
+    echo "[$(date)] Script encountered an error" >> $LOG_FILE
+fi
