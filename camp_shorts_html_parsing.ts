4f6b326d6f829413055407f3a44e13af524f5265
minsekim1 <tkarnrwl7862@naver.com>
2023-11-30 15:05:18 +0900

    숏폼 파싱추가

---
+++ camp_shorts_html_parsing.ts
@@ -0,0 +1,41 @@
+import { load } from "cheerio";
+import { html } from "./camp_shorts_html";
+import fs from "fs";
+
+const $ = load(html);
+
+const list: any = [];
+$("ytd-video-renderer").each((index, element) => {
+  const thumbnailUrl = $(element).find("ytd-thumbnail img").attr("src") || "";
+  const title = $(element).find("#video-title > yt-formatted-string.style-scope.ytd-video-renderer").text();
+  const videoLink = "https://www.youtube.com" + ($(element).find("a#video-title").attr("href") || "");
+  const viewCount = $(element).find(".inline-metadata-item").first().text();
+  const publishedDate = $(element).find(".inline-metadata-item").eq(1).text();
+  const authorName = $(element)
+    .find("yt-formatted-string > a.yt-simple-endpoint.style-scope.yt-formatted-string")
+    .first()
+    .text();
+  const authorImage = $(element).find("#channel-thumbnail img").attr("src") || "";
+  const description = $(element)
+    .find("yt-formatted-string.metadata-snippet-text.style-scope.ytd-video-renderer")
+    .text();
+
+  list.push(
+    `('${thumbnailUrl.replace(/'/g, "''")}', '${title.replace(/'/g, "''")}', '${videoLink.replace(
+      /'/g,
+      "''"
+    )}', '${viewCount.replace(/'/g, "''")}', '${publishedDate.replace(/'/g, "''")}', '${authorName.replace(
+      /'/g,
+      "''"
+    )}', '${authorImage.replace(/'/g, "''")}', '${description.replace(/'/g, "''")}')`
+  );
+});
+
+const sqlQuery = `INSERT INTO shorts (thumbnail_url, title, video_link, view_count, published_date, author_name, author_image, description) VALUES ${list.join(
+  ", "
+)};`;
+
+fs.writeFile("camp_shorts_html.log", sqlQuery, (err) => {
+  if (err) throw err;
+  console.log("The file has been saved!");
+});
