9b01ecb984539dcd99e610b98192f7e647570273
minsekim1 <tkarnrwl7862@naver.com>
2024-01-29 10:57:40 +0900

    오류 수정완료

--- function/common.ts
+++ function/common.ts
@@ -1,5 +1,5 @@
 export const isLocalhost = false; /// default: false
 
 export const NODE_API_URL_YAGI = isLocalhost ? "http://localhost:3001" : "https://node2.yagiyagi.kr";
-export const NODE_API_URL_CAMP = isLocalhost ? "http://localhost:3001" : "https://node5.yagiyagi.kr";
+export const NODE_API_URL_CAMP = isLocalhost ? "http://localhost:4001" : "https://node5.yagiyagi.kr";
 export const toComma = (number: string | number) => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
