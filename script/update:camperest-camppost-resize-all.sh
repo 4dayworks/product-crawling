0f89e378099aa2339519456b1570f1fe2ca99fac
minsekim1 <tkarnrwl7862@naver.com>
2024-02-26 12:44:10 +0900

    .

--- script/update:camperest-camppost-resize-all.sh
+++ script/update:camperest-camppost-resize-all.sh
@@ -8,7 +8,6 @@ echo "[$(date)] Script started" > $LOG_FILE
 # export PATH="/Users/yagiyagi/.nvm/versions/node/v16.20.1/bin:$PATH"
 export PATH="/Users/yagiyagi/.nvm/versions/node/v18.19.0/bin/node:$PATH"
 
-#!/bin/bash
 for i in {1..60}
 do
    curl -X POST -H "Authorization: Bearer dirldirlvkdlxld41669197319627067" "https://node5.yagiyagi.kr/campsite/post/resize" >> $LOG_FILE 2>&1
