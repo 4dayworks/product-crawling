f783ac737c6968c9da4566bf9bc6f42e50ef39be
minsekim1 <tkarnrwl7862@naver.com>
2024-04-01 15:17:17 +0900

    to ubuntu

--- script/update:delete-search-used-function.sh
+++ script/update:delete-search-used-function.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOG_FILE="/Users/yagiyagi/projects/dayworks4/product-crawling/script/update:delete-search-used-function.log"
+LOG_FILE="/home/ubuntu/product-crawling/script/update:delete-search-used-function.log"
 
 echo "[$(date)] Script started" > $LOG_FILE
 
