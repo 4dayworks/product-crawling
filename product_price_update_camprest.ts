9a659bc70c39a707b54a1b5d4d6ed2cd663974ca
minsekim1 <tkarnrwl7862@naver.com>
2024-04-30 18:24:36 +0900

    log 만 출력용

--- product_price_update_camprest.ts
+++ product_price_update_camprest.ts
@@ -3,13 +3,16 @@ import { AuthorizationKey } from "./function/auth";
 import { updateByCampProduct } from "./camp/functions/all_update";
 axios.defaults.headers.common["Authorization"] = `Bearer ${AuthorizationKey()}`;
 
-// updateByProductId({page: 0, size: 3000, product_id_list: [12414,121213]});
-updateByCampProduct({
-  // page: 0,
-  // size: 6000,
-  productSelectedList: [101],
-  type: "all",
-});
+const productSelectedList: number[] = [
+  7026, 7027, 7028, 7029, 7030, 7032, 7164, 7165, 7166, 7307, 7308, 7309, 7310, 7311,
+];
+
+const isProcess = 0;
+if (isProcess)
+  updateByCampProduct({ productSelectedList, type: "all" }).then(() =>
+    console.log(productSelectedList.map((i) => "https://mobile.camperest.kr/product/" + i))
+  );
+else console.log(productSelectedList.map((i) => "https://mobile.camperest.kr/product/" + i));
 
 // 캠퍼레스트 제품 (판매처) 업데이트
 // yarn update:camperest
